a49679a43bc16dd919543a191f994457c2ebe4aa
Quade81 <quade81@gmail.com>
2016-10-13 16:55:44 -0500

    Cleaning

--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -4,39 +4,14 @@ using System.Collections;
 public class ArrowScript : MonoBehaviour
 {
     private Vector3 previousPosition;
-    public bool isWaitingArrow;
-    public bool isSelfTeleportingArrow;
-    public bool isFreezingArrow;
-    public bool isTeleportingGrabberArrow;
-    public bool isTeleportingTargetLocArrow;
-    public bool isExplosiveArrow;
-    public bool isMineArrow;
     private float forceAmt = 1000f;
-    public float freezeColRadius;
-    public Transform iceSpike;
-    public Transform teleportingGrabberArrow;
     public bool stopped;
-    public bool exploded;
-    public float explosionForce;
-    public float explosionRadius;
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (stopped && other.GetComponent<EnemyScript>() != null && isMineArrow && !exploded)
-        {
-            Explode();
-            exploded = true;
-        }
-    }
 
     void Start()
     {
         previousPosition = transform.position;
         GetComponent<Rigidbody>().useGravity = false;
-        if (!isWaitingArrow)
-            Launch();
-        if (isWaitingArrow)
-            BowScript.Instance.AddWaitingArrow(this);
+        Launch();
 
     }
 
@@ -58,14 +33,6 @@ public class ArrowScript : MonoBehaviour
                     Vector3 newScale = hit.transform.localScale;
                     transform.localScale = new Vector3(1 / newScale.x, 1 / newScale.y, 1 / newScale.z);
                     GetComponent<Collider>().enabled = true;
-                    if (isFreezingArrow)
-                        StartCoroutine(Freeze());
-                    if (isSelfTeleportingArrow)
-                        TeleportPlayer();
-                    if (isTeleportingTargetLocArrow)
-                        Teleport();
-                    if (isExplosiveArrow)
-                        Explode();
                     if (hit.transform.root.GetComponent<EnemyScript>() != null)
                     {
                         hit.transform.root.GetComponent<EnemyScript>().Ragdoll();
@@ -87,53 +54,9 @@ public class ArrowScript : MonoBehaviour
         }
     }
 
-    public void Teleport()
-    {
-        if(teleportingGrabberArrow.transform.root.GetComponent<Rigidbody>() != null)
-            teleportingGrabberArrow.transform.root.position = transform.position;
-    }
-
-    public void Launch()
-    {
-        GetComponent<Rigidbody>().useGravity = true;
-        GetComponent<Rigidbody>().AddForce(transform.forward * BowScript.Instance.shotForce);
-    }
-
-    public void TeleportPlayer()
-    {
-        PlayerControlScript.Instance.transform.position = transform.position;
-    }
-
-    IEnumerator Freeze()
-    {
-        GetComponent<CapsuleCollider>().radius = 0.1f;
-        float scale = 0;
-        while (iceSpike.localScale.x < 1)
-        {
-            scale += Time.deltaTime;
-            iceSpike.localScale = Vector3.one * scale;
-            yield return null;
-        }
-        iceSpike.localScale = Vector3.one;
-    }
-
-    void Explode()
-    {
-        Collider[] itemsInRange = Physics.OverlapSphere(transform.position, explosionRadius);
-        foreach(Collider item in itemsInRange)
-        {
-            if (item.GetComponent<EnemyScript>() != null)
-            {
-                item.GetComponent<EnemyScript>().Ragdoll();
-            }
-        }
-        itemsInRange = Physics.OverlapSphere(transform.position, explosionRadius);
-        foreach (Collider item in itemsInRange)
-        {
-            if (item.GetComponent<Rigidbody>() != null)
-            {
-                item.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, transform.position, explosionRadius);
-            }
-        }
-    }
+	public void Launch()
+	{
+		GetComponent<Rigidbody>().useGravity = true;
+		GetComponent<Rigidbody>().AddForce(transform.forward * BowScript.Instance.shotForce);
+	}
 }
--- Assets/Scripts/BowScript.cs
+++ Assets/Scripts/BowScript.cs
@@ -1,26 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
 
 public class BowScript : MonoBehaviour
 {
-	public List<ArrowScript> waitingArrows = new List<ArrowScript>();
 	public float timeTilShot = 0;
 	public int shotForce;
-	public bool teleportingArrowOut = false;
-	public GameObject teleportingArrow;
-	public List<GameObject> arrowTypes = new List<GameObject>();
-	public int arrowType;
-	public Text arrowTypeName;
+	public GameObject arrowPrefab;
 
 	public static BowScript Instance;
 	void Awake() { Instance = this; }
-
-	void Start()
-	{
-		SwitchArrowType(0);
-	}
 
 	void Update()
 	{
@@ -31,60 +19,11 @@ public class BowScript : MonoBehaviour
 			Fire();
 			timeTilShot = 0.2f;
 		}
-		if (Input.GetButtonDown("Fire2"))
-		{
-			LaunchWaitingArrows();
-		}
-
-		if (Input.GetAxis("Mouse ScrollWheel") != 0)
-		{
-			SwitchArrowType((int)Mathf.Sign(Input.GetAxis("Mouse ScrollWheel")));
-		}
 	}
 
 	void Fire()
 	{
 		Transform camTransform = Camera.main.transform;
-		GameObject newArrow = Instantiate(arrowTypes[arrowType], (camTransform.forward * 1.15f) + camTransform.position, camTransform.rotation) as GameObject;
-		if (newArrow.GetComponent<ArrowScript>().isTeleportingGrabberArrow)
-		{
-			if (teleportingArrowOut)
-			{
-				newArrow.GetComponent<ArrowScript>().isTeleportingGrabberArrow = false;
-				newArrow.GetComponent<ArrowScript>().isTeleportingTargetLocArrow = true;
-				teleportingArrowOut = false;
-				newArrow.GetComponent<ArrowScript>().teleportingGrabberArrow = teleportingArrow.transform;
-			}
-			else
-			{
-				teleportingArrowOut = true;
-				teleportingArrow = newArrow;
-			}
-		}
-	}
-
-	void LaunchWaitingArrows()
-	{
-		foreach (ArrowScript arrow in waitingArrows)
-		{
-			arrow.Launch();
-		}
-		waitingArrows.Clear();
-	}
-
-	public void AddWaitingArrow(ArrowScript arrow)
-	{
-		waitingArrows.Add(arrow);
-	}
-
-	public void SwitchArrowType(int dir)
-	{
-		arrowType += dir;
-		if (arrowType < 0)
-			arrowType = arrowTypes.Count - 1;
-		if (arrowType > arrowTypes.Count - 1)
-			arrowType = 0;
-		arrowTypeName.text = arrowTypes[arrowType].name;
-
+		GameObject newArrow = Instantiate(arrowPrefab, (camTransform.forward * 1.15f) + camTransform.position, camTransform.rotation) as GameObject;
 	}
 }
